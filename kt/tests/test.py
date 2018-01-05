0d328a6461b621d7035071768bb20b263ecb4357
eaudemard <eric.audemard@gmail.com>
2018-01-05 09:52:58 -0500

    add tests framework

---
+++ kt/tests/test.py
@@ -0,0 +1,19 @@
+import unittest
+
+import os
+import sys
+
+
+class ktTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(True,
+                         True,
+                         "Test fail:")
+
+
+def runTests():
+    unittes.main()
+
+
+if __name__ == "__main__":
+    runTests()
